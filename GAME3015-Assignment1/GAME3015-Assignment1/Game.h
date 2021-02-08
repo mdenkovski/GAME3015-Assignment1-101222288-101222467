36e584a2e31ff9a42e0ddf545af0148ce331f70a
Michael Denkovski <michaeldenkovski@hotmail.com>
2021-02-08 17:41:33 -0500

    split into main

---
+++ GAME3015-Assignment1/GAME3015-Assignment1/Game.h
@@ -0,0 +1,135 @@
+#pragma once
+
+#include "../../Common/d3dApp.h"
+#include "../../Common/MathHelper.h"
+#include "../../Common/UploadBuffer.h"
+#include "../../Common/GeometryGenerator.h"
+#include "../../Common/Camera.h"
+#include "FrameResource.h"
+#include "Waves.h"
+#include "SceneNode.h"
+#include "World.h"
+#include <ctime>
+#include "Aircraft.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+//#pragma comment(lib, "d3dcompiler.lib")
+//#pragma comment(lib, "D3D12.lib")
+
+
+
+
+
+struct GameObject
+{
+	XMVECTOR position;
+	XMVECTOR velocity;
+	std::unique_ptr<RenderItem> renderItem;
+	int renderIndex;
+};
+
+class Game : public D3DApp
+{
+public:
+	Game(HINSTANCE hInstance);
+	Game(const Game& rhs) = delete;
+	Game& operator=(const Game& rhs) = delete;
+	~Game();
+
+	virtual bool Initialize()override;
+
+	int Start(HINSTANCE hInstance, HINSTANCE prevInstance,
+		PSTR cmdLine, int showCmd);
+
+private:
+	virtual void OnResize()override;
+	virtual void Update(const GameTimer& gt)override;
+	virtual void Draw(const GameTimer& gt)override;
+
+	virtual void OnMouseDown(WPARAM btnState, int x, int y)override;
+	virtual void OnMouseUp(WPARAM btnState, int x, int y)override;
+	virtual void OnMouseMove(WPARAM btnState, int x, int y)override;
+
+	void OnKeyboardInput(const GameTimer& gt);
+	//void UpdateCamera(const GameTimer& gt);
+	void AnimateMaterials(const GameTimer& gt);
+	void UpdateObjectCBs(const GameTimer& gt);
+	void UpdateMaterialCBs(const GameTimer& gt);
+	void UpdateMainPassCB(const GameTimer& gt);
+	void UpdateWaves(const GameTimer& gt);
+
+	void MoveGameObjects(const GameTimer& gt);
+
+	void LoadTextures();
+	void BuildRootSignature();
+	void BuildDescriptorHeaps();
+	void BuildShadersAndInputLayouts();
+
+	void BuildGroundGeometry();
+
+
+	void BuildPSOs();
+	void BuildFrameResources();
+	void BuildMaterials();
+	void BuildRenderItems();
+	void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems);
+
+	std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> GetStaticSamplers();
+
+	float GetHillsHeight(float x, float z)const;
+	XMFLOAT3 GetHillsNormal(float x, float z)const;
+
+private:
+
+	std::vector<std::unique_ptr<FrameResource>> mFrameResources;
+	FrameResource* mCurrFrameResource = nullptr;
+	int mCurrFrameResourceIndex = 0;
+
+	UINT mCbvSrvDescriptorSize = 0;
+
+	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
+
+	ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap = nullptr;
+
+	std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> mGeometries;
+	std::unordered_map<std::string, std::unique_ptr<Material>> mMaterials;
+	std::unordered_map<std::string, std::unique_ptr<Texture>> mTextures;
+	std::unordered_map<std::string, ComPtr<ID3DBlob>> mShaders;
+	std::unordered_map<std::string, ComPtr<ID3D12PipelineState>> mPSOs;
+
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mStdInputLayout;
+	std::vector<D3D12_INPUT_ELEMENT_DESC> mTreeSpriteInputLayout;
+
+	RenderItem* mWavesRitem = nullptr;
+
+	// List of all the render items.
+	std::vector<std::unique_ptr<RenderItem>> mAllRitems;
+
+	// Render items divided by PSO.
+	std::vector<RenderItem*> mRitemLayer[(int)RenderLayer::Count];
+	std::vector<BoundingBox> mBoundingBoxes;
+	std::vector<BoundingSphere> mBoundingSpheres;
+
+	std::unique_ptr<Waves> mWaves;
+
+	PassConstants mMainPassCB;
+
+	/*XMFLOAT3 mEyePos = { 0.0f, 0.0f, 0.0f };
+	XMFLOAT4X4 mView = MathHelper::Identity4x4();
+	XMFLOAT4X4 mProj = MathHelper::Identity4x4();
+
+	float mTheta = 1.5f * XM_PI;
+	float mPhi = XM_PIDIV2 - 0.1f;
+	float mRadius = 20.0f;*/
+
+
+	Camera mCamera;
+
+	POINT mLastMousePos;
+
+
+	World GameWorld;
+};
